8582a862cc4abcffcdb170d541e1aab3e4e11cbe
Lucas <lucassanchesfaustino3@hotmail.com>
2022-09-30 00:24:19 -0300

    Script inicial do projeto

---
+++ script_sexta.py
@@ -0,0 +1,36 @@
+from typing import Any
+import pandas as pd
+
+imported = pd.read_csv("caso_full.csv", sep=',');
+
+# print("---------------\n\n\n\n");
+# print(imported);
+
+#Isolando apenas para o Estado de SP
+db = imported[imported['state'] == 'SP'];
+
+# print("---------------\n\n\n\n");
+# print(db);
+
+# Exportando .csv de controle
+# db.to_csv(r'C:\Users\lucas\Desktop\trabalho-sexta\dados_isolados.csv', index=False);
+
+db1 = db.drop(['city_ibge_code', 'state', 'estimated_population_2019', 'epidemiological_week'], axis=1, inplace=True);
+print(db1);
+print("---------------\n\n\n\n");
+
+db1 = db[db['place_type'] == 'city']
+print(db1);
+print("---------------\n\n\n\n");
+
+# db1.to_csv(r'C:\Users\lucas\Desktop\trabalho-sexta\dados_nulos_mas_limpos.csv', index=False);
+
+# Remove qualquer linha que possua valores nulos, independente de qual valor esteja nulo.
+db1.dropna(axis=0, how='any', inplace=True);
+
+print(db1)
+
+# Exportando .csv de controle
+db1.to_csv(r'C:\Users\lucas\Desktop\trabalho-sexta\dados_limpos.csv', index=False);
+
+
